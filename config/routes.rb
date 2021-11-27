652e1fce0594912344d3019271e2e2d116797a3b
Luis Cordova <luis.cordova.olavarria@gmail.com>
2021-11-27 18:56:58 -0300

    cambio

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'prospects#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :prospects do
+  resources :prospects ,except: [:index]do
     resources :conversations, only: [:new, :create]
   end
 end
